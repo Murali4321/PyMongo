a8d4fdf01c12fde41563377c5eaa40be5ac75ee6
Murali4321 <49519090+Murali4321@users.noreply.github.com>
2019-04-11 17:25:45 +0530

    Add files via upload

--- PySMTP_2.py
+++ PySMTP_2.py
@@ -7,71 +7,79 @@ from email.mime.base import MIMEBase
 from email import encoders
 from email.mime.application import MIMEApplication
 
-path=input("Please enter path in the correct format to view files(e.g,C:\\Users\\Murali\\Pictures\\Files\\):")
+
 
 class files():
     def __init__(self):
         pass
         
     def view_files(self):
+        self.path=input("Please enter path where files to be displayed are present in correct format e.g,C:\\Users\\Murali\\Pictures\\Files\\:")
         print("Below files are present in the directory")
-        print("#########################################")
-        files = [f for f in glob.glob(path + "**/*.*", recursive=True)]
+        print("######################################################")
+        files = [f for f in glob.glob(self.path + "**/*.*", recursive=True)]
         for f in files:
             print(f)
         
     def user_interests(self):
+        print("#####################################################")
         print("Enter 1 for creating a new file")
         print("Enter 2 for deleting an existing file")
         print("Enter 3 for extracing and sending the text files in an email as attachments:")
         Choice=int(input("Please enter your choice:"))
-        return Choice        
+        return Choice
+    def execution(self,Choice):
+        if Choice==1:
+            filename=input("Please enter the file name:")
+            path=input("Please specify the path where you want to create this file in correct format(e.g,C:\\Users\\Murali\\Pictures\\Files\\):")
+            content=input("Please enter the content of the file:")
+            if not os.path.exists(path):
+                os.mkdir(path)
+            file = open(path+filename, 'w') 
+            file.write(content) 
+            file.close()
+            print("File created!!")    
+        elif Choice==2:
+            filename=input("please enter the file name you want to delete:")
+            if os.remove(self.path+filename):
+                print("File Removed!")
+            else:
+                print("Incorrect file name!!")    
+        
+    
+        elif Choice==3:
+            fromaddr = "murali52q7@gmail.com"
+            toaddr = "murali52q7@gmail.com"
+            msg = MIMEMultipart()
+            msg['From'] = fromaddr
+            msg['To'] = toaddr
+            msg['Subject'] = "Files for reference"
+            body = "Hi,\n\nPlease find the files attached\n\nThanks,\nMuralidhar"
+            msg.attach(MIMEText(body, 'plain'))
+            ftype=input("Please enter file extension-e.g, txt,pdf etc:")
+            files=[f for f in glob.glob(self.path + "**/*."+ftype, recursive=True)]
+            password=input("Please enter password of your email account:")
+            for file in files:
+                attachment = open(file, "rb")
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload((attachment).read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition', "attachment; filename= %s" % file)
+                msg.attach(part)
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(fromaddr,password)
+            text = msg.as_string()
+            server.sendmail(fromaddr, toaddr, text)
+            server.quit()
+            print("Sent an email with attachments!!")  
+
+        else:
+            print("Please verify your choice. Its incorrect!")              
+             
         
 Obj=files()
 
 Obj.view_files()
 Choice=Obj.user_interests()
-if Choice==1:
-    filename=input("Please enter the file name:")
-    path=input("Please specify the path where you want to create this file in correct format(e.g,C:\\Users\\Murali\\Pictures\\Files\\):")
-    content=input("Please enter the content of the file:")
-    if not os.path.exists(path):
-        os.mkdir(path)
-    file = open(path+filename, 'w') 
-    file.write(content) 
-    file.close()
-    print("File created!!")    
-elif Choice==2:
-    filename=input("please enter the file name you want to delete:")
-    os.remove(path+filename)
-    print("File Removed!")
-        
-    
-elif Choice==3:
-    fromaddr = "murali52q7@gmail.com"
-    toaddr = "murali52q7@gmail.com"
-    msg = MIMEMultipart()
-    msg['From'] = fromaddr
-    msg['To'] = toaddr
-    msg['Subject'] = "Files for reference"
-    body = "Hi,\n\nPlease find the files attached\n\nThanks,\nMuralidhar"
-    msg.attach(MIMEText(body, 'plain'))
-    files=[f for f in glob.glob(path + "**/*.txt", recursive=True)]
-    for file in files:
-        attachment = open(file, "rb")
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', "attachment; filename= %s" % file)
-        msg.attach(part)
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(fromaddr,"zxcvasd1")
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
-    server.quit()
-    print("Sent an email with attachments!!")  
-
-else:
-    print("Please verify your choice. Its incorrect!")              
- 
+Obj.execution(Choice)
--- Pymongo_1.py
+++ Pymongo_1.py
@@ -1,61 +1,68 @@
 
 from pymongo import MongoClient
 
-try: 
-    conn = MongoClient() 
-    print("Connected successfully to MongoDB!!!") 
-    print("###########################")
-except:   
-    print("Could not connect to MongoDB") 
 
-db=conn.Employee
-collection=db.Empl_details
 
 class Pymongo_:
     def __init__(self):
         pass
         
-    def current_docs(selfJ):
+    def connect(self):
+        try: 
+            conn = MongoClient() 
+            print("Connected successfully to MongoDB!!!")
+            print("##########################################") 
+        except:   
+            print("Could not connect to MongoDB") 
+
+        db=conn.Employee
+        self.collection=db.Empl_details   
+        
+    def current_docs(self):
         print("Current documents in the collection") 
-        cursor = collection.find()
+        cursor = self.collection.find()
         for record in cursor: 
             print(record)
         
     def user_interests(self):
+        print("#####################################")
         print("Enter 1 for inserting a new document")
         print("Enter 2 for updating existing document")
         print("Enter 3 for deleting document")
         Choice=int(input("Please enter your choice:"))
         return Choice
+    
+    def execution(self,Choice):        
+        if Choice==1:
+            Obj.current_docs()
+            id=int(input("Please enter employee id:"))
+            name=input("Please enter employee name:")
+            Ph=int(input("Please enter employee's phone number:"))
+            Addr=input("Please enter employee's address:")
+            employee={"_id":id,"name":name,"Phone":Ph,"Address":Addr}
+            records = Obj.collection.insert_one(employee)
+            Obj.current_docs()
+        
+        elif Choice==2:
+            Obj.current_docs()
+            updtkey=int(input("Please enter the id of the employee who needs updation:"))
+            new_addr=input("Please enter the new address of the employee:")
+            result = self.collection.update_one({"_id":updtkey},{"$set":{"Address":new_addr}})
+            Obj.current_docs()   
+
+
+        elif Choice==3:
+            Obj.current_docs()
+            delkey=int(input("Please enter the id of the employee who needs to be deleted:"))
+            result=self.collection.delete_one({"_id":delkey})
+            Obj.current_docs()
+        else:
+            print("Please verify your choice. Its incorrect!")    
 
 Obj=Pymongo_()
-
+Obj.connect()
+Obj.current_docs()
 Choice=Obj.user_interests()
+Obj.execution(Choice)
 
-        
-if Choice==1:
-    Obj.current_docs()
-    id=int(input("Please enter employee id:"))
-    name=input("Please enter employee name:")
-    Ph=int(input("Please enter employee's phone number:"))
-    Addr=input("Please enter employee's address:")
-
-    employee={"_id":id,"name":name,"Phone":Ph,"Address":Addr}
-    records = collection.insert_one(employee)
-    Obj.current_docs()
     
-elif Choice==2:
-    Obj.current_docs()
-    updtkey=int(input("Please enter the id of the employee who needs updation:"))
-    new_addr=input("Please enter the new address of the employee:")
-    result = collection.update_one({"_id":updtkey},{"$set":{"Address":new_addr}})
-    Obj.current_docs()   
-
-
-elif Choice==3:
-    Obj.current_docs()
-    delkey=int(input("Please enter the id of the employee who needs to be deleted:"))
-    result=collection.delete_one({"_id":delkey})
-    Obj.current_docs()
-else:
-    print("Please verify your choice. Its incorrect!")    
